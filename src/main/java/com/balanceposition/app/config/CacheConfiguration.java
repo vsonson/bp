e902fee3a9e3b2af7ebb1666bacac13984d83db9
mwquarles <michael@siliconharborlabs.com>
2018-04-18 22:40:20 -0400

    quotes

--- src/main/java/com/balanceposition/app/config/CacheConfiguration.java
+++ src/main/java/com/balanceposition/app/config/CacheConfiguration.java
@@ -47,6 +47,7 @@ public class CacheConfiguration {
             cm.createCache(com.balanceposition.app.domain.User.class.getName() + ".authorities", jcacheConfiguration);
             cm.createCache(com.balanceposition.app.domain.SocialUserConnection.class.getName(), jcacheConfiguration);
             cm.createCache(com.balanceposition.app.domain.UserInfo.class.getName(), jcacheConfiguration);
+            cm.createCache(com.balanceposition.app.domain.QuoteOfTheDay.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { BalancepositionUserInfoModule } from './user-info/user-info.module';
+import { BalancepositionQuoteOfTheDayModule } from './quote-of-the-day/quote-of-the-day.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
         BalancepositionUserInfoModule,
+        BalancepositionQuoteOfTheDayModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
